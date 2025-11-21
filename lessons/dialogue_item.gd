6b07fc135947da4be8eca6905dcb161699a867d7
Joshua <jobartolo@s.sfusd.edu>
2025-11-21 14:18:35 -0800

    M08L7

--- lessons/dialogue_item.gd
+++ lessons/dialogue_item.gd
@@ -1,3 +1,5 @@
-class_name DialogueItem extends SlideShowEntry
+@icon("res://assets/dialogue_item_icon.svg")
 
+class_name DialogueItem extends SlideShowEntry
+@export_group("Choices")
 @export var choices: Array[DialogueChoice] = []
